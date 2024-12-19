5e9ad9ed832752de7b229db6f6bc7128ed63c0d2
Axel8555 <10234443+Axel8555@users.noreply.github.com>
2024-12-19 11:02:14 -0600

    Reseed main tables

--- misc/reseed.sql
+++ misc/reseed.sql
@@ -1,3 +1,15 @@
+-- Borrar todos los registros y reiniciar el índice de rel_location_label
+DELETE FROM rel_location_label;
+DBCC CHECKIDENT ('rel_location_label', RESEED, 0);
+
+-- Borrar todos los registros y reiniciar el índice de his_product_movement
+DELETE FROM his_product_movement;
+DBCC CHECKIDENT ('his_product_movement', RESEED, 0);
+
+-- Borrar todos los registros y reiniciar el índice de tbl_production_batch
+DELETE FROM tbl_production_batch;
+DBCC CHECKIDENT ('tbl_production_batch', RESEED, 0);
+
 -- Borrar todos los registros y reiniciar el índice de tbl_product
 DELETE FROM tbl_product;
 DBCC CHECKIDENT ('tbl_product', RESEED, 0);
